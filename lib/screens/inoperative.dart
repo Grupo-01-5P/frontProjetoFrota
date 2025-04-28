2d8b7a58bbc4ce2b5dafd45b90a1f7f48c93f5c1
Kelvin Oliveira <kelvin.oliveira.kjo@gmail.com>
2025-04-27 22:27:06 -0300

    ajuste da sidebar

--- lib/screens/inoperative.dart
+++ lib/screens/inoperative.dart
@@ -9,80 +9,95 @@ class Inoperative extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       key: _scaffoldKey,
-      backgroundColor: Color.fromARGB(255, 250, 250, 250), // Cor de fundo #FAFAFA
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
       drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            UserAccountsDrawerHeader(
-              accountName: const Text('Kelvin'),
-              accountEmail: const Text('Editar minhas informações'),
-              currentAccountPicture: CircleAvatar(
-                backgroundColor: const Color.fromARGB(250, 250, 250, 250),
-                child: const Icon(Icons.person, size: 40, color: Colors.grey),
-              ),
-              decoration: const BoxDecoration(color: Colors.red),
-            ),
-            _buildDrawerItem(
-              icon: Icons.request_quote,
-              text: 'Orçamentos',
-              onTap: () {
-                // Ação para Orçamentos
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.build,
-              text: 'Visualizar manutenções',
-              onTap: () {
-                // Ação para Visualizar manutenções
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.warning,
-              text: 'Veículos inoperantes',
-              onTap: () {
-                Navigator.pop(context); // Fecha o Drawer sem criar nova tela
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.bar_chart,
-              text: 'Dashboards',
-              onTap: () {
-                // Ação para Dashboards
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.store,
-              text: 'Mecânicas',
-              onTap: () {
-                // Ação para Mecânicas
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.directions_car,
-              text: 'Veículos',
-              onTap: () {
-                // Ação para Veículos
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.settings,
-              text: 'Configurações',
-              onTap: () {
-                // Ação para Configurações
-              },
-            ),
-            _buildDrawerItem(
-              icon: Icons.exit_to_app,
-              text: 'Sair',
-              iconColor: Colors.red,
-              onTap: () {
-                // Ação para Sair
-              },
-            ),
-          ],
+  shape: RoundedRectangleBorder(
+    borderRadius: BorderRadius.zero, // Remove os arredondamentos
+  ),
+  child: ListView(
+    padding: EdgeInsets.zero,
+    children: [
+      UserAccountsDrawerHeader(
+        accountName: Text(
+          'Kelvin',
+          style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),
         ),
+        accountEmail: Text(
+          'Editar minhas informações',
+          style: TextStyle(color: const Color.fromARGB(255, 255, 255, 255)),
+        ),
+        currentAccountPicture: CircleAvatar(
+          backgroundColor: Color(0xFFD9D9D9),
+          child: Icon(Icons.person, size: 40, color: Colors.white),
+        ),
+        decoration: const BoxDecoration(color: Color(0xFF148553)),
       ),
+      _buildDrawerItem(
+        icon: Image.asset('lib/assets/images/iconTerceirize.png', width: 24, height: 24),
+        text: 'Orçamentos',
+        onTap: () {
+          // Ação para Orçamentos
+        },
+      ),
+      SizedBox(height: 8),
+      _buildDrawerItem(
+        icon: Image.asset('lib/assets/images/iconManutencoes.png', width: 24, height: 24),
+        text: 'Visualizar manutenções',
+        onTap: () {
+          // Ação para Visualizar manutenções
+        },
+      ),
+      SizedBox(height: 8),
+      _buildDrawerItem(
+        icon: Image.asset('lib/assets/images/iconInoperantes.png', width: 24, height: 24),
+        text: 'Veículos inoperantes',
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+      SizedBox(height: 8),
+      _buildDrawerItem(
+        icon: Image.asset('lib/assets/images/iconDashboard.png', width: 24, height: 24),
+        text: 'Dashboards',
+        onTap: () {
+          // Ação para Dashboards
+        },
+      ),
+      SizedBox(height: 8),
+      _buildDrawerItem(
+        icon: Image.asset('lib/assets/images/iconMecanica.png', width: 24, height: 24),
+        text: 'Mecânicas',
+        onTap: () {
+          // Ação para Mecânicas
+        },
+      ),
+      SizedBox(height: 8),
+      _buildDrawerItem(
+        icon: Image.asset('lib/assets/images/iconCar.png', width: 24, height: 24),
+        text: 'Veículos',
+        onTap: () {
+          // Ação para Veículos
+        },
+      ),
+      SizedBox(height: 8),
+      _buildDrawerItem(
+        icon: Image.asset('lib/assets/images/iconEngrenagem.png', width: 24, height: 24),
+        text: 'Configurações',
+        onTap: () {
+          // Ação para Configurações
+        },
+      ),
+      SizedBox(height: 8),
+      _buildDrawerItem(
+        icon: Image.asset('lib/assets/images/iconExit.png', width: 24, height: 24),
+        text: 'Sair',
+        onTap: () {
+          // Ação para Sair
+        },
+      ),
+    ],
+  ),
+),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
@@ -90,53 +105,39 @@ class Inoperative extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  // Botão para abrir o drawer
-                  IconButton(
-                    icon: const Icon(Icons.menu, color: Colors.black),
-                    onPressed: () {
-                      _scaffoldKey.currentState?.openDrawer();
-                    },
-                  ),
-                  // Botão de notificações
-                  Stack(
-                    children: [
-                      IconButton(
-                        icon: const Icon(Icons.notifications, color: Colors.black),
-                        onPressed: () {
-                          // Ação para notificações
-                        },
-                      ),
-                      Positioned(
-                        right: 8,
-                        top: 8,
-                        child: Container(
-                          padding: const EdgeInsets.all(4),
-                          decoration: const BoxDecoration(
-                            color: Colors.red,
-                            shape: BoxShape.circle,
-                          ),
-                          child: const Text(
-                            '3',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 12,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+  children: [
+    Container(
+      padding: const EdgeInsets.all(4),
+      decoration: BoxDecoration(
+        color: Colors.white, // Círculo branco
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.2), // Cor do sombreado
+            blurRadius: 6, // Intensidade do sombreado
+            offset: Offset(2, 2), // Posição do sombreado
+          ),
+        ],
+      ),
+      child: IconButton(
+        icon: Image.asset(
+          'lib/assets/images/iconMenu.png', // Caminho para a imagem do ícone de menu
+          width: 24,
+          height: 24,
+        ),
+        onPressed: () {
+          _scaffoldKey.currentState?.openDrawer();
+        },
+      ),
+    ),
+    // Adicione o segundo ícone ou outros widgets aqui, se necessário
+  ],
+),
               const SizedBox(height: 20),
 
-              // Card de buscar veículo
               Card(
-                color: Colors.white, // Fundo branco
-                elevation: 8, // Sombreamento
+                color: Colors.white,
+                elevation: 8,
                 shadowColor: Colors.black.withOpacity(0.2),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(12),
@@ -146,12 +147,11 @@ class Inoperative extends StatelessWidget {
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      // Parte de cima: título + ícone
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           Padding(
-                            padding: const EdgeInsets.only(left: 12), // deixei 12px pra ficar bem alinhado
+                            padding: const EdgeInsets.only(left: 12),
                             child: Text(
                               "Buscar veículo",
                               style: TextStyle(
@@ -163,23 +163,20 @@ class Inoperative extends StatelessWidget {
                           IconTheme(
                             data: IconThemeData(
                               color: Colors.black,
-                              size: 28, // deixa maior
+                              size: 28,
                             ),
                             child: Icon(Icons.sort_by_alpha),
                           ),
                         ],
                       ),
-
                       const SizedBox(height: 20),
-
-                      // Apenas o campo de texto
                       TextField(
                         decoration: InputDecoration(
                           filled: true,
-                          fillColor: Color(0xFFEEEEEE), // Fundo cinza claro
+                          fillColor: Color(0xFFEEEEEE),
                           hintText: "Digite a placa do veículo",
                           border: OutlineInputBorder(
-                            borderSide: BorderSide.none, // Sem borda
+                            borderSide: BorderSide.none,
                             borderRadius: BorderRadius.circular(8),
                           ),
                           contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
@@ -190,87 +187,75 @@ class Inoperative extends StatelessWidget {
                 ),
               ),
 
-              // Novo Card de veículo com informações
-              // Novo card com ajustes no status
-Card(
-  color: Colors.white,
-  elevation: 8,
-  shadowColor: Colors.black.withOpacity(0.2),
-  shape: RoundedRectangleBorder(
-    borderRadius: BorderRadius.circular(6), // Aumento no arredondamento
-  ),
-  child: Padding(
-    padding: const EdgeInsets.all(16),
-    child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        // Nome da placa e modelo
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "RDM4J56", // Nome da placa
-              style: TextStyle(
-                fontWeight: FontWeight.w900,
-                fontSize: 20, // Aumenta o tamanho da fonte
-              ),
-            ),
-            Text(
-              "Toyota Corolla XEi 2022", // Modelo do veículo
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-          ],
-        ),
-        const SizedBox(height: 10),
-
-        // Cor
-        Text(
-          "Prata",
-          style: TextStyle(fontSize: 14),
-        ),
-        const SizedBox(height: 8),
-
-        // Empresa
-        Text(
-          "Logística Express Ltda.",
-          style: TextStyle(fontSize: 14),
-        ),
-        const SizedBox(height: 8),
-
-        // Departamento
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              "Frota Operacional",
-              style: TextStyle(fontSize: 14),
-            ),
-            // Adicionando espaçamento entre o card de status e o card de informações
-            SizedBox(width: 16), // Ajuste o valor de espaçamento aqui
-            // Status ao lado do Departamento, com espaçamento
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
-              decoration: BoxDecoration(
-                color: Color(0xFFFFAC26).withOpacity(0.7),
-                borderRadius: BorderRadius.circular(20), // Maior arredondamento
-              ),
-              child: Text(
-                "Inoperante", // Texto do status
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 14,
+              Card(
+                color: Colors.white,
+                elevation: 8,
+                shadowColor: Colors.black.withOpacity(0.2),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "RDM4J56",
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 20,
+                            ),
+                          ),
+                          Text(
+                            "Toyota Corolla XEi 2022",
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "Prata",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      const SizedBox(height: 8),
+                      Text(
+                        "Logística Express Ltda.",
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      const SizedBox(height: 8),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "Frota Operacional",
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          SizedBox(width: 16),
+                          Container(
+                            padding: const EdgeInsets.symmetric(vertical: 6, horizontal: 12),
+                            decoration: BoxDecoration(
+                              color: Color(0xFFFFAC26).withOpacity(0.7),
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Text(
+                              "Inoperante",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-      ],
-    ),
-  ),
-)
-
             ],
           ),
         ),
@@ -306,24 +291,24 @@ Card(
               items: [
                 BottomNavigationBarItem(
                   icon: SizedBox(
-                    width: 30,  // Largura desejada
-                    height: 30, // Altura desejada
+                    width: 30,
+                    height: 30,
                     child: Image.asset('lib/assets/images/iconManutencoes.png'),
                   ),
                   label: 'Manutenções',
                 ),
                 BottomNavigationBarItem(
                   icon: SizedBox(
-                    width: 30,  // Largura desejada
-                    height: 30, // Altura desejada
+                    width: 30,
+                    height: 30,
                     child: Image.asset('lib/assets/images/iconTerceirize.png'),
                   ),
                   label: 'Orçamentos',
                 ),
                 BottomNavigationBarItem(
                   icon: SizedBox(
-                    width: 30,  // Largura desejada
-                    height: 30, // Altura desejada
+                    width: 30,
+                    height: 30,
                     child: Image.asset('lib/assets/images/iconInoperantes.png'),
                   ),
                   label: 'Inoperantes',
@@ -337,13 +322,12 @@ Card(
   }
 
   Widget _buildDrawerItem({
-    required IconData icon,
+    required Widget icon,
     required String text,
     required VoidCallback onTap,
-    Color iconColor = Colors.red,
   }) {
     return ListTile(
-      leading: Icon(icon, color: iconColor),
+      leading: icon,
       title: Text(text),
       onTap: onTap,
     );
