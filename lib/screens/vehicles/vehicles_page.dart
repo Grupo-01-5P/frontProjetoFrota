342a16fb801de5cd03f790f7efecd0213468d2f8
João Vitor Candido <joaovitorcand@outlook.com>
2025-05-14 20:38:49 -0300

    me achando

--- lib/screens/vehicles/vehicles_page.dart
+++ lib/screens/vehicles/vehicles_page.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:front_projeto_flutter/screens/budgets/budgets_listage.dart';
 import 'package:front_projeto_flutter/screens/budgets/budgets_page.dart';
-import 'package:front_projeto_flutter/screens/vehicles/vehicles_page.dart';
 
-class HomePage extends StatelessWidget {
-  HomePage({super.key});
+class VehiclesPage extends StatelessWidget {
+  VehiclesPage({super.key});
 
   // Criação de uma GlobalKey para controlar o Scaffold
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
@@ -93,16 +93,9 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
+
       body: Stack(
         children: [
-          // Background da tela
-          Positioned.fill(
-            child: Image.asset(
-              'lib/assets/images/backgroundHome.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-
           // Botões no topo
           SafeArea(
             child: Padding(
@@ -154,28 +147,82 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
-
-          // Conteúdo principal
-          SafeArea(
+          // Botões logo abaixo do cabeçalho
+          Padding(
+            padding: EdgeInsets.only(
+              top: kToolbarHeight + 32, // altura do appBar + espaço extra
+              left: 16,
+              right: 16,
+            ),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const SizedBox(height: 15), // Reduzido o espaço acima dos cards
-                // Cards com input de placa
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.20,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => BudgetsListage(),
+                        ),
+                      );
+                    },
                     child: Column(
-                      mainAxisAlignment:
-                          MainAxisAlignment
-                              .end, // Move os cards para perto da barra inferior
-                      children: const [
-                        _PlacaCard(titulo: "Aprovar solicitação"),
-                        SizedBox(height: 12),
-                        _PlacaCard(titulo: "Visualizar manutenção do veículo"),
-                        SizedBox(height: 12),
-                        _PlacaCard(titulo: "Deslocamento de veículo"),
-                        SizedBox(height: 12),
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Cadastrar um veículo',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        const SizedBox(height: 8),
+                        SvgPicture.asset(
+                          'lib/assets/images/carromais.svg',
+                          width: 24,
+                          height: 24,
+                          color: Colors.green,
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16), // Espaço entre os botões
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.20,
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => BudgetsListage(),
+                        ),
+                      );
+                    },
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                          'Visualizar a base de veículos',
+                          style: TextStyle(fontSize: 24),
+                        ),
+                        const SizedBox(height: 8),
+                        SvgPicture.asset(
+                          'lib/assets/images/carro.svg',
+                          width: 24,
+                          height: 24,
+                          color: Colors.green,
+                        ),
                       ],
                     ),
                   ),
@@ -222,41 +269,6 @@ class HomePage extends StatelessWidget {
       leading: Icon(icon, color: iconColor),
       title: Text(text),
       onTap: onTap,
-    );
-  }
-}
-
-class _PlacaCard extends StatelessWidget {
-  final String titulo;
-
-  const _PlacaCard({required this.titulo});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      elevation: 4,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(titulo, style: const TextStyle(fontWeight: FontWeight.bold)),
-            const SizedBox(height: 8),
-            const TextField(
-              decoration: InputDecoration(
-                hintText: 'Digite a placa do veículo',
-                filled: true,
-                fillColor: Color(0xFFF1F1F1),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide.none,
-                  borderRadius: BorderRadius.all(Radius.circular(8)),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
